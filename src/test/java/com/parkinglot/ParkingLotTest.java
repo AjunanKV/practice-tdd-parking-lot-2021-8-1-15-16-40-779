76a300005d752272a2bc4a621ab75616b7836f9e
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-06 15:50:26 +0800

    [ParkingLot-Story1][Kevin]test: add test case: should_return_nothing_when_fetch_car_given_parking_lot_wrong_ticket_number()

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -1,12 +1,12 @@
 package com.parkinglot;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     @Test
-    public void should_return_parking_ticken_when_park_given_parking_lot_and_car(){
+    public void should_return_parking_ticken_when_park_given_parking_lot_and_car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
@@ -19,7 +19,7 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void should_return_car_when_fetch_given_parking_lot_and_parked_car_and_parking_ticket(){
+    public void should_return_car_when_fetch_given_parking_lot_and_parked_car_and_parking_ticket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
@@ -29,11 +29,11 @@ public class ParkingLotTest {
         Car actualCar = parkingLot.fetch(parkingTicket);
 
         //then
-        assertEquals(car,actualCar);
+        assertEquals(car, actualCar);
     }
 
     @Test
-    public void should_return_right_car_when_fetch_twice_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets(){
+    public void should_return_right_car_when_fetch_twice_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         Car aliceCar = new Car();
@@ -46,13 +46,21 @@ public class ParkingLotTest {
         Car actualBobCar = parkingLot.fetch(bobParkingTicket);
 
         //then
-        assertEquals(aliceCar,actualAliceCar);
-        assertEquals(bobCar,actualBobCar);
+        assertEquals(aliceCar, actualAliceCar);
+        assertEquals(bobCar, actualBobCar);
     }
 
+    @Test
+    public void should_return_nothing_when_fetch_car_given_parking_lot_wrong_ticket_number() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        ParkingTicket invalidParkingTicket = new ParkingTicket();
 
-
-
-
+        //when
+        Car actualCar = parkingLot.fetch(invalidParkingTicket);
+        //then
+        assertNull(actualCar);
+    }
 
 }
