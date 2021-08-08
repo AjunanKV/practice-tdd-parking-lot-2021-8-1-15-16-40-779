e8b1121c6a92eb9bcb489c8fdf3d5d47de17344d
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-08 20:27:40 +0800

    [ParkingLot][Kevin]chore: add SuperSmartParkingBoy.java

---
+++ src/main/java/com/parkinglot/SuperSmartParkingBoy.java
@@ -0,0 +1,13 @@
+package com.parkinglot;
+
+import java.util.List;
+
+public class SuperSmartParkingBoy  extends StandardParkingBoy{
+    public SuperSmartParkingBoy(ParkingLot parkingLots) {
+        super(parkingLots);
+    }
+
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+}
