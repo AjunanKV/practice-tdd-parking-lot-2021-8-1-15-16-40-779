ec1870562c39da8efd95cc8938d2bcdf12e4a746
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-08 20:42:35 +0800

    [ParkingLot][Kevin]feat: add implementation to: should_return_car_given_super_smart_parking_boy_a_parking_lot_and_car()

--- src/main/java/com/parkinglot/SuperSmartParkingBoy.java
+++ src/main/java/com/parkinglot/SuperSmartParkingBoy.java
@@ -1,13 +1,28 @@
 package com.parkinglot;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class SuperSmartParkingBoy  extends StandardParkingBoy{
-    public SuperSmartParkingBoy(ParkingLot parkingLots) {
-        super(parkingLots);
+public class SuperSmartParkingBoy extends StandardParkingBoy{
+
+    public SuperSmartParkingBoy(ParkingLot parkingLot) {
+        super(parkingLot);
     }
 
     public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
+    }
+
+
+    public ParkingTicket park(Car car) {
+        return getAvailableSmartParkingLot().park(car);
+    }
+
+    private ParkingLot getAvailableSmartParkingLot(){
+        return getParkingLots()
+                .stream()
+                .filter(parkingLot -> !parkingLot.ifFull())
+                .max(Comparator.comparing(ParkingLot::getCurrentParkedCarsCount))
+                .orElseThrow(ParkingWithNoPosition::new);
     }
 }
